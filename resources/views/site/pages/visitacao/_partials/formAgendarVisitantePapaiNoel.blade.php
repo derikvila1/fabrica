425ecad9618fa5dfb89c67b390d21cc4936bd279
Derik Vila <derikvila@gmail.com>
2022-12-09 11:00:04 -0400

    ad

--- resources/views/site/pages/visitacao/_partials/formAgendarVisitantePapaiNoel.blade.php
+++ resources/views/site/pages/visitacao/_partials/formAgendarVisitantePapaiNoel.blade.php
@@ -39,7 +39,7 @@
                             style="width: 90%; max-width: 250px; text-align: center;">
 			</div>
 
-				<!--<div class="col-md-12">
+				<div class="col-md-12">
 					<div class="py-5 text-center">
 						<h2>Bem vindo</h2>
 						<p class="lead pt-2">
@@ -50,8 +50,9 @@
 							<a href="\consulta">Clique aqui!</a>
 						</div>
 					</div>
-				</div>-->
-				 <div class="col-md-12">
+				</div>
+
+				 <!-- <div class="col-md-12">
 					<div class="py-5 text-center">
 						<h3>AGENDAMENTO DA SEMANA ESGOTADO</h3>
 						<p class="lead pt-2">
@@ -68,11 +69,11 @@
 					</div>
 				</div> 
 
-			</div>
+			</div> -->
 
 		</header>
         <main class="visitante d-print-none">
-			<!--
+			
             <section class="container">
 				<div class="row">
 					<div class="col-md-12">
@@ -274,7 +275,7 @@
 		</main>
     </form> {{-- fim formulario principal--}}
     
-    -->
+   
         <footer class="my-5 pt-5 text-muted text-center text-small d-print-none">
 			<p class="mb-1">Secretaria de Cultura e Economia Criativa</p>
 			<p class="mb-1">Desenvolvimento: Inovação e Tecnologia Audivisual</p>
